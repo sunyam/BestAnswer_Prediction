1accf5709b16fbd38b403533596f14e6692914af
Sunyam Bagga <sunyambagga@gmail.com>
2018-03-19 15:34:10 -0400

    changing naming convention

--- prepare_txts.py
+++ prepare_txts.py
@@ -1,17 +1,44 @@
+'''
+   Writing the 'Body' (or Body+Title) of the CSV file into separate .txt files, so word2vec can process them. 
+'''
+
 import pandas as pd
 
-def write_to_txt(input_path, output_path):
+def write_questions_to_txt(input_path, output_path, name):
+    python_df = pd.read_csv(input_path)
+    python_df['All_Body'] = python_df['Title'] + python_df['Body']
+    txtPython = python_df['All_Body'].tolist()
 
+    for i, body in enumerate(txtPython[:10]):
+        #    print body
+        cleanBody = unicode(body, errors='ignore')
+        with open(output_path+'question_'+name+'_'+str(i+1)+'.txt', 'wb') as f:
+            f.write(cleanBody)
+
+
+def write_answers_to_txt(input_path, output_path, name):
     python_df = pd.read_csv(input_path)
     txtPython = python_df['Body'].tolist()
 
-    for i, body in enumerate(txtPython):
+    for i, body in enumerate(txtPython[:10]):
         #    print body
         cleanBody = unicode(body, errors='ignore')
-        with open(output_path+'python_'+str(i+1)+'.txt', 'wb') as f:
+        with open(output_path+'answer_'+name+'_'+str(i+1)+'.txt', 'wb') as f:
             f.write(cleanBody)
 
 if __name__ == '__main__':
-    input_path = '../Dataset/pythonquestions/procPythonAnswers.csv'
+    # NOTE: If preparing txt(s) for pythonquestions, change name variable to 'python' and the paths obviously.
+    # name variable is to make sure it doesn't overwrite an existing txt file.
+    
+    # 1) For answers:
+    input_path = '../Dataset/stacksample/procStacksampleAnswers.csv'
     output_path = '../Dataset/TXT/'
-    write_to_txt(input_path, output_path)
+    name = 'stack'
+    write_answers_to_txt(input_path, output_path, name)
+
+    # 2) For questions:
+    inp_path = '../Dataset/stacksample/Questions.csv'
+    out_path = '../Dataset/TXT/'
+    name = 'stack'
+    write_questions_to_txt(inp_path, out_path, name)
+
