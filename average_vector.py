f9bc2f2d7d7229b6d9bbed44590b8d41f2c73bef
Sunyam Bagga <sunyambagga@gmail.com>
2018-03-19 21:28:19 -0400

    Adding questions.csv functionality

--- average_vector.py
+++ average_vector.py
@@ -42,27 +42,40 @@ def calculate_vector(body):
 
 if __name__ == '__main__':
     
+    # Set this flag to be True if processing Questions.csv; False for Answers.csv:
+    Q_FLAG = True
+
+    df_path = '../Dataset/pythonquestions/Questions.csv'
+    python_df = pd.read_csv(df_path)
+    
     # Loading Word2Vec model:
     path_to_model = '/Users/sunyambagga/Desktop/SO_model.word2vec'
     model = gensim.models.Word2Vec.load(path_to_model)
-    
-    # Pick ID and Body columns from the CSV:
-    df_path = '../Dataset/pythonquestions/procPythonAnswers.csv'
-    python_df = pd.read_csv(df_path)
-    df = python_df[['Id', 'Body']]
+
+    # If dealing with questions:
+    if Q_FLAG:
+        print "Processing the Questions ...."
+        python_df['NewBody'] = python_df['Title'] + python_df['Body']
+        df = python_df[['Id', 'NewBody']]
+        output_filename = 'SO_Questions_vectors.pickle'
+
+    else:
+        print "Processing the Answers ...."
+        df = python_df[['Id', 'Body']]
+        output_filename = 'SO_Answers_vectors.pickle'
     
     # Have to dropna() because 2 bodies were 'nan' in Answers.csv
     df.dropna(inplace=True)
 
     # Dictionary to map ID to vector:
-    answers_id_vec = {}
+    map_id_to_vec = {}
     
     tuples = [tuple(x) for x in df.values]
     k = 0
     for (ID, body) in tuples:
         cleanBody = unicode(body, errors='ignore')
         vec = calculate_vector(cleanBody)
-        answers_id_vec[ID] = vec
+        map_id_to_vec[ID] = vec
         
         # After removing nan's, should be no more floats (checking anyway)
         if type(body) == float:
@@ -75,6 +88,6 @@ if __name__ == '__main__':
     print "Null vector (vector of three hundred 0's) count: ", null_vector_count
 
     # Pickle the average-vectors for later use:
-    with open('/Users/sunyambagga/Desktop/SO_feature_vectors.pickle', 'wb') as f:
-        pickle.dump(answers_id_vec, f)
-    print "\nSuccessfully pickled " + str(len(answers_id_vec)) + " posts."
+    with open('/Users/sunyambagga/Desktop/' + output_filename, 'wb') as f:
+        pickle.dump(map_id_to_vec, f)
+    print "\nSuccessfully pickled " + str(len(map_id_to_vec)) + " posts."
